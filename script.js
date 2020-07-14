548efcdd968b79e4ae99b07935abc6d6827053ea
Ayoola David <ayooladavid@gmail.com>
2020-07-14 20:03:25 +0100

    repo rebase

--- script.js
+++
@@ -1,8 +0,0 @@
-$(".bingo-card__item").on('click', function() {
-  $(this).toggleClass('active');
-  console.log("click")
-});
-
-$('.clear-button').on('click', function(){
-	$('.bingo-card__item').removeClass('active');
-});
